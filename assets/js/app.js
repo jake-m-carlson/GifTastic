c65d4349320c88b46b7ccdb2160210fac39102a6
Jake Carlson <jakecarlson@jakes-mbp.lan>
2019-08-05 18:58:04 -0500

    pseduo coded js & html

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+// Page loads w/ some buttons already entered
+// Array w/ preloaded button keywords
+// when a word is typed in form a new button will populate
+// when a button is clicked, 10 gifs will appear as still images
+// each gif will have its rating displayed
+// when a gif is clicked individually, it will be an active gif
+// when a gif is clicked again it will be a still image
+// 
+// 
+// 
+// 
+// 
