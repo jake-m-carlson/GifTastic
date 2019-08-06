85718dc4507c368243e0928e946c6f279bf34c0a
Jake Carlson <jakecarlson@jakes-mbp.lan>
2019-08-05 23:07:02 -0500

    added url with api key and desination

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,23 +2,23 @@
 $(document).ready(function() {
 
 // Array w/ preloaded button keywords
-    var loadedDestinations = ["Bali", "Mykonos", "Bora Bora", "Phuket", "Galapagos Islands"];
-    console.log(loadedDestinations);
+    var topics = ["Bali", "Mykonos", "Bora Bora", "Phuket", "Galapagos Islands"];
+    console.log(topics);
 
     // function to display destination data
     function makeButton() {
         //removes destinations before adding new ones
         $("#buttons").empty();
         // for loop through array of destinations
-        for (var i = 0; i < loadedDestinations.length; i++) {
+        for (var i = 0; i < topics.length; i++) {
             // used to create butten beginning & end tags
             var a = $("<button>");
             // add class to button
             a.addClass("destinations");
             // add data attribute
-            a.attr("data-name", loadedDestinations[i]);
+            a.attr("data-name", topics[i]);
             // adding text to the button
-            a.text(loadedDestinations[i]);
+            a.text(topics[i]);
             // add a button to the buttons div 
             $("#buttons").append(a);
             
@@ -27,15 +27,25 @@ $(document).ready(function() {
 
 // when a word is typed in form a new button will populate
     $("#add-destination").on("click", function() {
-        // save new destination in var loadedDestinations
+        // save new destination in var topics
         var newDestination = $("#destination-input").val().trim();
         // add destination from input text
-        loadedDestinations.push(newDestination);
+        topics.push(newDestination);
         // run makeButton function to make new button for new movie
         makeButton();
         event.preventDefault(); //***************** Diff from this or return false; ??? */
     })
+
 // when a button is clicked, 10 gifs will appear as still images
+    function displayGifs() {
+        $("#images").empty();
+        // api set up
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + destination + "&api_key=" + apiKey + "&limit=10";
+        var destination = $(this).attr("data-name");
+        var apiKey = "ePu4sAeJ3pnzZxSW4crhvYCK0zZwNzug";
+        console.log(destination);
+        displayGifs()
+    } 
 // each gif will have its rating displayed
 // when a gif is clicked individually, it will be an active gif
 // when a gif is clicked again it will be a still image
